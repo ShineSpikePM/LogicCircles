1f049dc9a7e5ce4a4d380946d6d6b85ab45c6ac3
ShineSpike <matiasmw3@gmail.com>
2021-02-10 23:00:22 -0500

    added new scripts and sprite

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    // back is right
+    // forward is left
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed);
+        transform.Rotate(Vector3.back);
+    }
+}
